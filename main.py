d9624a545a2d9d60587f7c0379c1ac84035aca0d
JA-RgueZ <rguez@ieee.org>
2026-03-02 01:08:58 -0300

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import gspread
 from fastapi.middleware.cors import CORSMiddleware
 
 # --- METADATA DEL PROYECTO ---
-VERSION = "1.1.4-stable"
+VERSION = "1.1.5-stable"
 app = FastAPI(title="FEDRO API", version=VERSION)
 
 # --- Configuración CORS ---
